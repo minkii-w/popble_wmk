4279cf9d1535b07b8e5571de039d0cc934513949
PC <PC@DESKTOP-G2AEOVT>
2025-08-26 15:02:07 +0900

    commit test

---
+++ popble/src/main/java/com/popble/PopbleApplication.java
@@ -0,0 +1,13 @@
+package com.popble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopbleApplication.class, args);
+	}
+
+}
---
+++ popble/src/test/java/com/popble/PopbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
