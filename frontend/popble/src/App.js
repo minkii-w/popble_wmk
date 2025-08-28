d3c10cb0052cb2bf183acc312809035ae2d1649c
PC <PC@DESKTOP-G2AEOVT>
2025-08-28 15:33:07 +0900

    firsttest

---
+++ frontend/popble/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-extrabold">안녕하세요</h1>
+    </div>
+  );
+}
+
+export default App;
