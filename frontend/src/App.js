154ccc0412649751b4f3c1bfc2e0f40cfbe54759
jaehyeong <sbcream1988@gmail.com>
2025-08-29 18:00:57 +0900

    front add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,31 @@ import './App.css';
 
 function App() {
   return (
-    <div className="text-5xl">
-      <h1>안녕</h1>
+    <div>
+      <div className="text-5xl bg-primaryColor">
+        <h1>안녕</h1>
+      </div>
+      <div className="text-5xl bg-secondaryColor">
+        <h1>안녕</h1>
+      </div>
+      <div className="text-5xl bg-secondaryAccentColor">
+        <h1>안녕</h1>
+      </div>
+      <div className="text-5xl bg-backgroundColor">
+        <h1>안녕</h1>
+      </div>
+      <div className="text-5xl bg-subFirstColor">
+        <h1>안녕</h1>
+      </div>
+      <div className="text-5xl bg-subSecondColor">
+        <h1>안녕</h1>
+      </div>
+      <div className="text-5xl bg-subButtonColor">
+        <h1>안녕</h1>
+      </div>
+      <div className="text-5xl bg-subButtonAccentColor">
+        <h1>안녕</h1>
+      </div>
     </div>
   );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,8 +5,15 @@ module.exports = {
   ],
   theme: {
     extend: {
-      theme:{
-        
+      colors:{
+        primaryColor:"#B2D4FD",
+        secondaryColor:"#E4DFFF",
+        secondaryAccentColor:"#D0C6FF",
+        backgroundColor:"#FFFCF8",
+        subFirstColor:"#E9E9E9",
+        subSecondColor:"#D9D9D9",
+        subButtonColor:"#FFD6A5",
+        subButtonAccentColor:"#FFB6B9"
       }
     },
   },
