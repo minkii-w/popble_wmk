e3d83a68e70dfa3ca3ada9ba8c3558a1078a22c0
sbcream1988 <sbcream1988@gmail.com>
2025-09-03 21:52:52 +0900

    repository 추가수정

--- backend/src/main/java/com/popble/repository/BookmarkRepository.java
+++ backend/src/main/java/com/popble/repository/BookmarkRepository.java
@@ -8,6 +8,8 @@ import com.popble.domain.UserProfile;
 import java.util.List;
 import java.util.Optional;
 import com.popble.domain.PopupStore;
+import java.time.LocalDateTime;
+
 
 
 
@@ -15,8 +17,9 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Long>{
 
 	//유저의 북마크
 	List<Bookmark> findByUserProfile(UserProfile userProfile);
-	
-	
+	//북마크한 팝업스토어를 최신순으로
+	List<Bookmark> findByUserProfileOrderByCreateDateDesc(UserProfile userProfile);
+	//특정 유저가 북마크를 했는지 안했는지 여부
 	Optional<Bookmark> findByUserProfileAndPopupStore(UserProfile userProfile, PopupStore popupStore);
 	
 		
--- backend/src/main/java/com/popble/repository/PopupStoreRepository.java
+++ backend/src/main/java/com/popble/repository/PopupStoreRepository.java
@@ -1,5 +1,6 @@
 package com.popble.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -15,12 +16,15 @@ public interface PopupStoreRepository extends JpaRepository<PopupStore, Long>{
 	//특정 카테고리에 속한 팝업스토어 조회
 	
 	//삭제되지 않은 팝업만 조회
-	
+	List<PopupStore> findAllByDeleted(boolean deleted);
 	//추천수가 높은 순으로 정렬
-	
+	List<PopupStore> findAllByOrderByRecommendDesc();
 	//특정 주소에 위치한 팝업스토어 찾기
-	List<PopupStore> findByBookmarkCountOrderByDesc(int bookmarkCount, String desc);
-	//북마크 순으로 정렬
 	
+	//북마크 많은 순으로 정렬
+	List<PopupStore> findAllByOrderByBookmarkCountDesc();
 	//조회순으로 정렬
+	List<PopupStore> findAllByOrderByViewDesc();
+	//종료 임박순
+	List<PopupStore> findAllByEndDateAfterOrderByEndDateAsc(LocalDate endDate);
 }
